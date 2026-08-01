480ec23093658d93224ad588f2befa6954a0a7f6
kushal thakkar <kushalthakkar2024@gmail.com>
2026-08-01 15:28:56 +0530

    !help ignored

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ intents.message_content = True
 intents.members = True
 intents.voice_states = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 # Global debug mode flag
 debug_mode = False
